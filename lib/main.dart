c914a6eae9ae18c8fe9e2ccd7bc19dd1c24e2dbb
sajanaka00 <nivin.2019452@iit.ac.lk>
2021-08-30 09:42:01 +0530

    Added a ListView to overcome the overflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -73,113 +74,120 @@ class _MyHomePageState extends State<MyApp> {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(25.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: ListView(
                       children: <Widget>[
-                        SizedBox(height: 20),
-                        Text("Account Number", style: TextStyle(fontSize: 14.0),),
-                        SizedBox(height: 10),
-                        TextField(
-                          controller: accNumController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: '67588881',
-                            isDense: true,
-                            contentPadding: EdgeInsets.all(12),
-                            hintStyle: TextStyle(fontSize: 14.0)
-                          ),
-                        ),
-                        SizedBox(height: 15),
-                        Text("Receiver Account Number", style: TextStyle(fontSize: 14.0),),
-                        SizedBox(height: 10),
-                        TextField(
-                          controller: receiverAccNumController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Receiver Account Number',
-                            // prefixText: '675',
-                              prefixIcon: Container(
-                                color: Colors.black12,
-                                child: Container(
-                                  margin: EdgeInsets.all(15),
-                                  child: Text("675"),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(height: 20),
+                            Text("Account Number", style: TextStyle(fontSize: 14.0),),
+                            SizedBox(height: 10),
+                            TextField(
+                              controller: accNumController,
+
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: '67588881',
+                                isDense: true,
+                                contentPadding: EdgeInsets.all(12),
+                                hintStyle: TextStyle(fontSize: 14.0)
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Text("Receiver Account Number", style: TextStyle(fontSize: 14.0),),
+                            SizedBox(height: 10),
+                            TextField(
+                              controller: receiverAccNumController,
+
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'Receiver Account Number',
+                                // prefixText: '675',
+                                  prefixIcon: Container(
+                                    color: Colors.black12,
+                                    child: Container(
+                                      margin: EdgeInsets.all(15),
+                                      child: Text("675"),
+                                    ),
+                                  ),
+                                isDense: true,
+                                  contentPadding: EdgeInsets.all(15),
+                                  prefixStyle: TextStyle(backgroundColor: Colors.black12),
+                                  hintStyle: TextStyle(fontSize: 14.0)
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Text("Re-Enter Receiver Account Number", style: TextStyle(fontSize: 14.0),),
+                            SizedBox(height: 10),
+                            TextField(
+                              controller: reEnterReceiverAccNumController,
+
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'Receiver Account Number',
+                                prefixIcon: Container(
+                                  color: Colors.black12,
+                                  child: Container(
+                                    margin: EdgeInsets.all(15),
+                                    child: Text("675"),
+                                  ),
+                                ),
+                                isDense: true,
+                                  contentPadding: EdgeInsets.all(15),
+                                  prefixStyle: TextStyle(backgroundColor: Colors.black12),
+                                  hintStyle: TextStyle(fontSize: 14.0)
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Text("Amount", style: TextStyle(fontSize: 14.0),),
+                            SizedBox(height: 10),
+                            TextField(
+                              controller: amountController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'Amount',
+                                isDense: true,
+                                  contentPadding: EdgeInsets.all(12),
+                                  hintStyle: TextStyle(fontSize: 14.0)
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Checkbox(
+                                  checkColor: Colors.white,
+                                  activeColor: Colors.blue,
+                                  value: this.checkboxValue,
+                                  onChanged: (bool value) {
+                                    setState(() {
+                                      this.checkboxValue = value;
+                                    });
+                                    },
+                                ),
+                                Text("I agree to the Terms and Conditions", style: TextStyle(fontSize: 15.0),)
+                              ],
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: 16.0),
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 45,
+                                child: RaisedButton(
+                                  child: Text("Proceed", style: TextStyle(color: Colors.white, fontSize: 16)),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  onPressed: () {
+                                    proceedData();
+                                  },
+                                  color: Colors.blue,
                                 ),
                               ),
-                            isDense: true,
-                              contentPadding: EdgeInsets.all(15),
-                              prefixStyle: TextStyle(backgroundColor: Colors.black12),
-                              hintStyle: TextStyle(fontSize: 14.0)
-                          ),
-                        ),
-                        SizedBox(height: 15),
-                        Text("Re-Enter Receiver Account Number", style: TextStyle(fontSize: 14.0),),
-                        SizedBox(height: 10),
-                        TextField(
-                          controller: reEnterReceiverAccNumController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Receiver Account Number',
-                            prefixIcon: Container(
-                              color: Colors.black12,
-                              child: Container(
-                                margin: EdgeInsets.all(15),
-                                child: Text("675"),
-                              ),
                             ),
-                            isDense: true,
-                              contentPadding: EdgeInsets.all(15),
-                              prefixStyle: TextStyle(backgroundColor: Colors.black12),
-                              hintStyle: TextStyle(fontSize: 14.0)
-                          ),
-                        ),
-                        SizedBox(height: 15),
-                        Text("Amount", style: TextStyle(fontSize: 14.0),),
-                        SizedBox(height: 10),
-                        TextField(
-                          controller: amountController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Amount',
-                            isDense: true,
-                              contentPadding: EdgeInsets.all(12),
-                              hintStyle: TextStyle(fontSize: 14.0)
-                          ),
-                        ),
-                        SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Checkbox(
-                              checkColor: Colors.white,
-                              activeColor: Colors.blue,
-                              value: this.checkboxValue,
-                              onChanged: (bool value) {
-                                setState(() {
-                                  this.checkboxValue = value;
-                                });
-                                },
-                            ),
-                            Text("I agree to the Terms and Conditions", style: TextStyle(fontSize: 15.0),)
                           ],
                         ),
-                        Container(
-                          padding: EdgeInsets.only(top: 16.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            height: 45,
-                            child: RaisedButton(
-                              child: Text("Proceed", style: TextStyle(color: Colors.white, fontSize: 16)),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)
-                              ),
-                              onPressed: () {
-                                proceedData();
-                              },
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ),
-                      ],
+                      ]
                     ),
                   ),
                 ),
@@ -207,5 +215,7 @@ class _MyHomePageState extends State<MyApp> {
     reEnterReceiverAccNumController.clear();
     amountController.clear();
   }
+
+
 
 }
